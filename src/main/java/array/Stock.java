508c5eddf6f31b4c7de1193ebfe54ead0f73633f
lanweibin <lanweibin0203@outlook.com>
2017-11-15 19:31:29 +0800

    reverse String

--- src/main/java/array/Stock.java
+++ src/main/java/array/Stock.java
@@ -16,10 +16,59 @@ public class Stock {
         return  maxshouyi;
     }
 
+//    public  static  int maxProfit2(int[] prices){
+//        if(prices == null || prices.length <2){
+//            return 0;
+//        }
+//
+//        int temp = prices[0];
+//        int next ;
+//        int sum = 0 ;
+//        for (int i = 0; i < prices.length-1; i++) {
+//            next = prices[i+1];
+//            if(next>temp){
+//                sum+= next - temp;
+//                temp = next;
+//            }else {
+//                next = temp;
+//            }
+//        }
+//
+//        return sum;
+//
+//
+//    }
+
+
+    public static int maxProfit3(int[] prices) {
+        if(prices==null||prices.length<2){
+            return 0;
+        }
+        int temp=prices[0];
+        int pre;
+        int sum=0;
+        for(int i=0;i<prices.length-1;i++){
+            pre=prices[i+1];
+            if(pre>temp){
+                sum+=(pre-temp);
+                temp=pre;
+            }else{
+                temp=pre;
+            }
+        }
+        return sum;
+    }
+
+
     public static void main(String[] args) {
         int[] prices = {7, 1, 4, 3, 9};
         int n =maxProfit(prices);
 
+      //  int m = maxProfit2(prices);
+
+        int m = maxProfit3(prices);
+
         System.out.println(n);
+        System.out.println(m);
     }
 }
---
+++ src/main/java/string/ReverseString.java
@@ -0,0 +1,19 @@
+package string;
+
+public class ReverseString {
+    public static String reverseString(String s) {
+        char[] c = s.toCharArray();
+        int j = 0;
+        for (int i = s.length()-1; i>=0; i--) {
+            c[j++] = s.charAt(i);
+
+        }
+        return  new String(c);
+    }
+
+    public static void main(String[] args) {
+        String s = "abc";
+
+        System.out.println(reverseString(s));
+    }
+}
