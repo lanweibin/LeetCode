6895f5a6c053cf73f3afe5351e87e750363bfb09
lanweibin <lanweibin0203@outlook.com>
2017-11-16 17:31:29 +0800

    Detect Capital

---
+++ src/main/java/string/DetectCapital.java
@@ -0,0 +1,24 @@
+package string;
+
+public class DetectCapital {
+    public static boolean detectCapitalUse(String word) {
+        String up = word.toUpperCase();
+        String low = word.toLowerCase();
+
+        if(up.equals(word) || low.equals(word)){
+            return true;
+        }
+
+        String other = word.substring(1,word.length());
+        if(other.toLowerCase().equals(other)){
+            return  true;
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        String word = "UUA";
+
+        System.out.println(detectCapitalUse(word));
+    }
+}
