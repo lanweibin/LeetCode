aaee1ba3b3d2c91c1329b5aaa62f3f50a42bfa9c
lanweibin <lanweibin0203@outlook.com>
2017-11-18 14:54:44 +0800

    reverse strings

---
+++ src/main/java/string/ReverseStrings.java
@@ -0,0 +1,36 @@
+package string;
+
+public class ReverseStrings {
+    public static String reverseStr(String s, int k) {
+        char[] c = s.toCharArray();
+        int n = c.length;
+        int i = 0;
+
+
+
+        while(i < n) {
+            int j = Math.min(i + k - 1, n - 1);
+            swap(c, i, j);
+            i += 2 * k;
+        }
+        return  String.valueOf(c);
+
+    }
+
+    public static void swap(char[] arr, int l, int r){
+        while(l<r){
+            char temp = arr[l];
+            arr[l++] = arr[r];
+            arr[r--] = temp;
+        }
+    }
+
+
+    public static void main(String[] args) {
+        String s = "abcd";
+        String result = reverseStr(s,4);
+
+        System.out.println(result);
+
+    }
+}
