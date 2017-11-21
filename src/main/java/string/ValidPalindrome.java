d4818acd5d593e8a15b15799c1efc12cdeae2bd2
lanweibin <lanweibin0203@outlook.com>
2017-11-21 19:22:46 +0800

    字符回文判断

---
+++ src/main/java/string/ValidPalindrome.java
@@ -0,0 +1,35 @@
+package string;
+
+public class ValidPalindrome {
+    public static boolean isPalindrome(String s) {
+        String ss = s.toLowerCase();
+        char[] c = ss.toCharArray();
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < s.length(); i++) {
+            if(c[i]>='a' && c[i]<='z'){
+                sb.append(c[i]);
+            }
+            if(c[i]>='0' && c[i]<='9'){
+                sb.append(c[i]);
+            }
+
+        }
+        String allchar = sb.toString();
+        String resvers = sb.reverse().toString();
+
+        if (allchar.equals(resvers)){
+            return true;
+        }
+
+        return false;
+    }
+
+    public static void main(String[] args) {
+        String s = "0p";
+
+        boolean b = isPalindrome(s);
+
+        System.out.println(b);
+    }
+}
