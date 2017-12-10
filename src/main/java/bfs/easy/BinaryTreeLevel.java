7897da2456597e384b7461f0e9960e804e44f730
lanweibin <lanweibin0203@outlook.com>
2017-12-10 23:21:05 +0800

    leetcode练习

--- src/main/java/bfs/easy/BinaryTreeLevel.java
+++ src/main/java/bfs/easy/BinaryTreeLevel.java
@@ -1,4 +1,25 @@
 package bfs.easy;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class BinaryTreeLevel {
+    public List<List<Integer>> levelOrder(TreeNode root){
+        List<List<Integer>> result = new LinkedList<>();
+
+        bfs(result,root, 0);
+
+        return result;
+    }
+
+    private void bfs(List<List<Integer>> result, TreeNode root,  int level) {
+        if (root == null) return;
+        if (level >= result.size()){
+            result.add(new LinkedList<>());
+        }
+
+        result.get(level).add(root.val);
+        bfs(result, root.left, level+1);
+        bfs(result, root.right, level+1);
+    }
 }
