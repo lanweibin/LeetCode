9eeda971cfdcab722bcc9da8de8680093cdc1d45
lanweibin <lanweibin0203@outlook.com>
2017-12-24 22:55:29 +0800

    练习

---
+++ src/main/java/array/medium/MaximumSwap.java
@@ -0,0 +1,34 @@
+package array.medium;
+
+import org.omg.PortableInterceptor.INACTIVE;
+
+//670 Maximum Swap
+public class MaximumSwap {
+    public static int maximumsSwap(int num) {
+        char[] digits = Integer.toString(num).toCharArray();
+
+        int[] buckets = new int[10];
+        for (int i = 0; i < digits.length; i++) {
+            buckets[digits[i] - '0'] = i;
+        }
+
+        for (int i = 0; i < digits.length; i++) {
+            for (int j = 9; j > digits[i] - '0'; j--) {
+                if (buckets[j] > i) {
+                    char tem = digits[i];
+                    digits[i] = digits[buckets[j]];
+                    digits[buckets[j]] = tem;
+                    return Integer.valueOf(new String(digits));
+                }
+
+            }
+
+        }
+        return num;
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println(maximumsSwap(1234));
+    }
+}
--- src/main/java/string/medium/DeleteOperation.java
+++ src/main/java/string/medium/DeleteOperation.java
@@ -3,28 +3,30 @@ package string.medium;
 //583. Delete Operation for Two Strings
 public class DeleteOperation {
     public static int minDistance(String word1, String word2) {
-        int i = 0, j = 0, count = 0;
+        int m = word1.length(), n = word2.length();
+        int[][] dp = new int[m+1][n+1];
 
-        while (i<=word1.length()-1){
-            if (word1.charAt(i) != word2.charAt(j)){
-                count++;
-                j++;
-            }else {
-                i++;
-                j++;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == 0 || j == 0){
+                    dp[i][j] = 0;
+                } else if  (word1.charAt(i-1) == word2.charAt(j-1)){
+                    dp[i][j] = dp[i-1][j-1] + 1;
+                }else {
+                    dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
+                }
+
             }
-        }
-        if (j <= word2.length()-1){
-            j++;
-            count++;
+
         }
 
-        return count;
+        int val =  dp[word1.length()][word2.length()];
+        return m+n - 2*val;
     }
 
     public static void main(String[] args) {
-        String s1 = "aaq";
-        String s2 = "eaat";
+        String s1 = "sea";
+        String s2 = "eat";
 
         int n = minDistance(s1,s2);
         System.out.println(n);
